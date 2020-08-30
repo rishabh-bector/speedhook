c65ae2b5f67d14246a218e64536cf1525004f407
Rishabh Bector <bector.rishabh@gmail.com>
2020-08-30 15:10:24 -0500

    added more speed options

--- popup.js
+++ popup.js
@@ -6,10 +6,20 @@ function speedScript(num) {
     `
 }
 
+let s0 = document.getElementById('s0');
 let s1 = document.getElementById('s1');
 let s2 = document.getElementById('s2');
 let s3 = document.getElementById('s3');
 let s4 = document.getElementById('s4');
+let s5 = document.getElementById('s5');
+
+s0.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: speedScript(0.25) });
+    });
+};
 
 s1.onclick = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -40,5 +50,13 @@ s4.onclick = function (element) {
         chrome.tabs.executeScript(
             tabs[0].id,
             { code: speedScript(2.0) });
+    });
+};
+
+s5.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: speedScript(10.0) });
     });
 };
